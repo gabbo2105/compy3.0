9d02737810b023793e0e23bc50d3a76572f73f42
gabbo2105 <115617800+gabbo2105@users.noreply.github.com>
2025-12-05 10:35:28 +0100

    Update webhook URL in app.js

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ async function sendMessage() {
   input.value = "";
 
   // 📡 Chiamata al tuo webhook n8n
-  const response = await fetch("https://innovasemplice.app.n8n.cloud/webhook-test/compy", {
+  const response = await fetch("https://innovasemplice.app.n8n.cloud/webhook/compy", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
